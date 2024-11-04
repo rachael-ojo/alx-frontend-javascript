ac080675d01ef2b272a5b1a723ffb7aca9cd8886
Rachael Ojo <adeolaatilade@gmail.com>
2024-11-04 21:27:51 +0100

    Updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,12 @@
-function taskBlock(trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // Using const to prevent overwriting the outer scope variable
-    const task2 = false; // Using const to prevent overwriting the outer scope variable
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
 }
-
-// Example usage:
-console.log(taskBlock(true)); // Expected output: [false, true]
-console.log(taskBlock(false)); // Expected output: [false, true]
